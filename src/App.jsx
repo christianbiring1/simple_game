dccb28b4f7b05798367c7bd0b143764fdef188d5
christian <christianbiringanine22@gmail.com>
2023-12-21 11:26:48 +0200

    Track the number of rolls it took to win the game

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
 
   const [dice, setDice] = useState(generateNumber());
   const [tenzies, setTenzies] = useState(false);
+  // number of rolls!
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     const allHeld = dice.every(die => die.isHeld);
@@ -16,11 +18,14 @@ function App() {
     const allSameValue = dice.every(die => die.value === firstValue);
     if (allHeld && allSameValue) {
       setTenzies(true);
-      console.log('You won')
     }
   }, [dice]);
 
   function generateNewDie() {
+    // let dots = "";
+    // for(let j = 1; j < Math.ceil(Math.random() * 6); j += 1) {
+    //   dots += ".";
+    // }
     return {
       value:  Math.floor(Math.random() * 6),
       isHeld: false,
@@ -53,17 +58,26 @@ function App() {
       setDice(prev => prev.map(die => {
         return die.isHeld ? die : generateNewDie()
       }));
+      setCount(prev => prev + 1);
     } else {
       setTenzies(false);
       setDice(generateNumber());
+      setCount(0)
     }
   }
+
+  // TO-DO
+  // * CSS: Put real dots on the dice instead of numbers :ok
+  // ** Track the number of rolls it tooks to win the game :0k
+  // *** Track the time it took to win the game
+  // **** Save the best time and/or the lowest number of rolls (try to beat your records)
 
   return (
     <main>
       {tenzies && <Confetti/>}
       <h1 className='title'>Dice Game</h1>
       <p className='instructions'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <span>{count}</span>
       <div className='dice-container'>
         {dice.map(die => (
           <Dice
